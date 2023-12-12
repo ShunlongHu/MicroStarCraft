93744db374796de0b16a8745fd21ab297e9bb148
zoe50 <zoe50@QIAQIA>
2023-12-12 12:35:21 +0800

    调整游戏参数到map

--- server/engine/game_types.h
+++ server/engine/game_types.h
@@ -13,34 +13,72 @@ constexpr static int GAME_PRODUCIBLE_OBJ_TYPE_NUM = 6;
 constexpr static int GAME_ACTION_TYPE_NUM = 6;
 constexpr static int GAME_DIRECTION_TYPE_NUM = 4;
 
+enum GameObjType {
+    TERRAIN,
+    MINERAL,
+    BASE,
+    BARRACK,
+    WORKER,
+    LIGHT,
+    HEAVY,
+    RANGED
+};
+
+const static std::unordered_map<GameObjType, int> OBJ_HP_MAP {  // sc2 / 25
+    {BASE, 60},
+    {BARRACK, 40},
+    {WORKER, 4},
+    {LIGHT, 4},
+    {HEAVY, 12},
+    {RANGED, 4}
+};
+
+const static std::unordered_map<GameObjType, int> OBJ_ATTACK_MAP { // sc2 / 40
+    {WORKER, 1},
+    {LIGHT, 1},
+    {HEAVY, 4},
+    {RANGED, 1}
+};
+
+const static std::unordered_map<GameObjType, int> OBJ_ATTACK_CD_MAP { // sc2 * 2
+        {WORKER, 1},
+        {LIGHT, 0},
+        {HEAVY, 1},
+        {RANGED, 0}
+};
+
+const static std::unordered_map<GameObjType, int> OBJ_COST_MAP { // sc2 / 50
+        {BASE, 16},
+        {BARRACK, 6},
+        {WORKER, 2},
+        {LIGHT, 1},
+        {HEAVY, 4},
+        {RANGED, 2}
+};
+
+const static std::unordered_map<GameObjType, int> OBJ_TIME_MAP { // sc2 / 4
+        {BASE, 25},
+        {BARRACK, 16},
+        {WORKER, 4},
+        {LIGHT, 3},
+        {HEAVY, 7},
+        {RANGED, 6}
+};
+
+const static std::unordered_map<GameObjType, int> OBJ_MOVE_INTERVAL_MAP { // (移动前摇) sc2 * 2
+        {WORKER, 1},
+        {LIGHT, 0},
+        {HEAVY, 1},
+        {RANGED, 1}
+};
+
 // GAME_ATTACK
 constexpr static int RADIUS_MELEE = 1;
 constexpr static int RADIUS_RANGED = 3;
-constexpr static int HP_BASE = 60; // starcraft building hp/25
-constexpr static int HP_BARRACK = 40;
-constexpr static int HP_WORKER = 2; // starcraft unit hp/25
-constexpr static int HP_LIGHT = 2;
-constexpr static int HP_HEAVY = 6;
-constexpr static int HP_RANGED = 2;
-constexpr static int ATTACK_WORKER = 1;
-constexpr static int ATTACK_LIGHT = 2;
-constexpr static int ATTACK_HEAVY = 2;
-constexpr static int ATTACK_RANGED = 1;
 
 // GAME_ECONOMY
 constexpr static int RES_PER_GATHER = 1;
-constexpr static int COST_BASE = 4;
-constexpr static int COST_BARRACK = 3;
-constexpr static int COST_WORKER = 2;
-constexpr static int COST_LIGHT = 1;
-constexpr static int COST_HEAVY = 4;
-constexpr static int COST_RANGED = 2;
-constexpr static int TIME_BASE = 36;
-constexpr static int TIME_BARRACK = 24;
-constexpr static int TIME_WORKER = 6; // scv
-constexpr static int TIME_LIGHT = 4;  // zergling
-constexpr static int TIME_HEAVY = 12;   // zelot
-constexpr static int TIME_RANGED = 9;  // marine
+constexpr static int GATHER_POST = 0;
 
 template <class T> struct UHasher{
     size_t operator() (const T& key) const {
@@ -58,17 +96,6 @@ template <class T> struct UHasher{
         }
         return std::_Hash_array_representation(reinterpret_cast<const char*>(&key), sizeof(key));
     }
-};
-
-enum GameObjType {
-    TERRAIN,
-    MINERAL,
-    BASE,
-    BARRACK,
-    WORKER,
-    LIGHT,
-    HEAVY,
-    RANGED
 };
 
 enum ActionType {
@@ -97,64 +124,33 @@ struct Coord {
 };
 using ActionTarget = Coord;
 
-const static std::vector<ActionTarget> DIRECTION_TARGET_MAP {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
-const static  std::vector<ActionTarget> ATTACK_RANGE_TARGET_MAP {
-        {-3, -3},
-        {-3, -2},
-        {-3, -1},
-        {-3, -0},
-        {-3, 1},
-        {-3, 2},
-        {-3, 3},
+// 攻击无法选择方向，以默认攻击方向进行攻击
+const static std::unordered_map<int, std::vector<Coord>> ATTACK_RANGE_COORD_MAP{
+        /*
+         *   x
+         *  xox
+         *   x
+         */
+        {1, {{-1, 0}, {1, 0}, {0, -1}, {0, 1}}},
+        /*   xxx
+         *  x o x
+         *   xxx
+         */
+        {2, {{1, 1}, {1, -1}, {-1, 1}, {-1, -1}, {-2, 0}, {2, 0}, {0, -2}, {0, 2}}},
+        /*
+         *    x
+         *  xx xx
+         *  x   x
+         * x  o  x
+         *  x   x
+         *  xx xx
+         *    x
+         */
+        {3, {{2, 1}, {2, -1}, {-2, 1}, {-2, -1}, {1, 2}, {1, -2}, {-1, 2}, {-1, -2}, {2, 2}, {2, -2}, {-2, 2}, {-2, -2}, {-3, 0}, {3, 0}, {0, -3}, {0, 3}}},
 
-        {-2, -3},
-        {-2, -2},
-        {-2, -1},
-        {-2, -0},
-        {-2, 1},
-        {-2, 2},
-        {-2, 3},
-
-        {-1, -3},
-        {-1, -2},
-        {-1, -1},
-        {-1, -0},
-        {-1, 1},
-        {-1, 2},
-        {-1, 3},
-
-        {0, -3},
-        {0, -2},
-        {0, -1},
-        {0, -0},
-        {0, 1},
-        {0, 2},
-        {0, 3},
-
-        {1, -3},
-        {1, -2},
-        {1, -1},
-        {1, -0},
-        {1, 1},
-        {1, 2},
-        {1, 3},
-
-        {2, -3},
-        {2, -2},
-        {2, -1},
-        {2, -0},
-        {2, 1},
-        {2, 2},
-        {2, 3},
-
-        {3, -3},
-        {3, -2},
-        {3, -1},
-        {3, -0},
-        {3, 1},
-        {3, 2},
-        {3, 3},
 };
+
+const static std::vector<ActionTarget> DIRECTION_TARGET_MAP {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
 
 struct GameObj {
     GameObjType type;
@@ -165,6 +161,7 @@ struct GameObj {
     ActionTarget actionTarget;
     int actionProgress;
     int actionTotalProgress;
+    int attackCD;
 };
 
 struct GameState {

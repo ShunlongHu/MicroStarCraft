3ea726ba3c753a0589ad9da64139989dd1e088bb
zoe50 <zoe50@QIAQIA>
2023-12-21 13:01:41 +0800

    try qt official

--- observer/ui/rts_map.cpp
+++ observer/ui/rts_map.cpp
@@ -18,8 +18,11 @@ using namespace std;
 GLfloat lightAmbient[4] = { 0.5, 0.5, 0.5, 1.0 };
 GLfloat lightDiffuse[4] = { 1.0, 1.0, 1.0, 1.0 };
 GLfloat lightPosition[4] = { 0.0, 0.0, 2.0, 1.0 };
+//
+//static glm::mat4 projection {glm::perspective(glm::radians(45.0f), 1.0f, 0.1f, 100.0f)};
+//static glm::mat4 view {1.0f};
+//static glm::mat4 model {1.0f};
 
-static glm::mat4 projection {glm::perspective(glm::radians(45.0f), 1.0f, 0.1f, 100.0f)};
 static GLuint ID;
 
 void RtsMap::checkCompileErrors(GLuint shader, std::string type)
@@ -56,76 +59,91 @@ void RtsMap::checkCompileErrors(GLuint shader, std::string type)
 void RtsMap::initializeGL() {
     QOpenGLWidget::initializeGL();
     initializeOpenGLFunctions();
-    loadGLTextures();
+//    loadGLTextures();
+//
+//    glEnable( GL_TEXTURE_2D );
+//    glShadeModel( GL_SMOOTH );
+//    glClearColor( 0.0, 0.0, 0.0, 0.5 );
+//    glClearDepth( 1.0 );
+//    glEnable( GL_DEPTH_TEST );
+//    glDepthFunc( GL_LEQUAL );
+//    glHint( GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST );
+//
+//    glLightfv( GL_LIGHT1, GL_AMBIENT, lightAmbient );
+//    glLightfv( GL_LIGHT1, GL_DIFFUSE, lightDiffuse );
+//    glLightfv( GL_LIGHT1, GL_POSITION, lightPosition );
+//
+//    glEnable( GL_LIGHT1 );
+//
+//    std::string vertexCode;
+//    std::string fragmentCode;
+//    std::ifstream vShaderFile;
+//    std::ifstream fShaderFile;
+//    // ensure ifstream objects can throw exceptions:
+//    vShaderFile.exceptions (std::ifstream::failbit | std::ifstream::badbit);
+//    fShaderFile.exceptions (std::ifstream::failbit | std::ifstream::badbit);
+//    try
+//    {
+//        // open files
+//        vShaderFile.open("D:\\repo\\rts\\observer\\ui\\coord_sys.vs");
+//        fShaderFile.open("D:\\repo\\rts\\observer\\ui\\coord_sys.fs");
+//        std::stringstream vShaderStream, fShaderStream;
+//        // read file's buffer contents into streams
+//        vShaderStream << vShaderFile.rdbuf();
+//        fShaderStream << fShaderFile.rdbuf();
+//        // close file handlers
+//        vShaderFile.close();
+//        fShaderFile.close();
+//        // convert stream into string
+//        vertexCode = vShaderStream.str();
+//        fragmentCode = fShaderStream.str();
+//    }
+//    catch (std::ifstream::failure& e)
+//    {
+//        std::cout << "ERROR::SHADER::FILE_NOT_SUCCESSFULLY_READ: " << e.what() << std::endl;
+//    }
+//    const char* vShaderCode = vertexCode.c_str();
+//    const char * fShaderCode = fragmentCode.c_str();
+//    // 2. compile shaders
+//    unsigned int vertex, fragment;
+//    // vertex shader
+//    vertex = glCreateShader(GL_VERTEX_SHADER);
+//    glShaderSource(vertex, 1, &vShaderCode, NULL);
+//    glCompileShader(vertex);
+//    checkCompileErrors(vertex, "VERTEX");
+//
+//    // fragment Shader
+//    fragment = glCreateShader(GL_FRAGMENT_SHADER);
+//    glShaderSource(fragment, 1, &fShaderCode, NULL);
+//    glCompileShader(fragment);
+//    checkCompileErrors(fragment, "FRAGMENT");
+//
+//    // shader Program
+//    ID = glCreateProgram();
+//    glAttachShader(ID, vertex);
+//    glAttachShader(ID, fragment);
+//    glLinkProgram(ID);
+//    checkCompileErrors(ID, "PROGRAM");
+//
+//    // delete the shaders as they're linked into our program now and no longer necessary
+//    glDeleteShader(vertex);
+//    glDeleteShader(fragment);
+//    glUseProgram(ID);
 
-    glEnable( GL_TEXTURE_2D );
-    glShadeModel( GL_SMOOTH );
-    glClearColor( 0.0, 0.0, 0.0, 0.5 );
-    glClearDepth( 1.0 );
-    glEnable( GL_DEPTH_TEST );
-    glDepthFunc( GL_LEQUAL );
-    glHint( GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST );
+    glClearColor(0, 0, 0, 1);
 
-    glLightfv( GL_LIGHT1, GL_AMBIENT, lightAmbient );
-    glLightfv( GL_LIGHT1, GL_DIFFUSE, lightDiffuse );
-    glLightfv( GL_LIGHT1, GL_POSITION, lightPosition );
+    initShaders();
+    initTextures();
 
-    glEnable( GL_LIGHT1 );
+//! [2]
+    // Enable depth buffer
+    glEnable(GL_DEPTH_TEST);
 
-    std::string vertexCode;
-    std::string fragmentCode;
-    std::ifstream vShaderFile;
-    std::ifstream fShaderFile;
-    // ensure ifstream objects can throw exceptions:
-    vShaderFile.exceptions (std::ifstream::failbit | std::ifstream::badbit);
-    fShaderFile.exceptions (std::ifstream::failbit | std::ifstream::badbit);
-    try
-    {
-        // open files
-        vShaderFile.open("D:\\repo\\rts\\observer\\ui\\coord_sys.vs");
-        fShaderFile.open("D:\\repo\\rts\\observer\\ui\\coord_sys.fs");
-        std::stringstream vShaderStream, fShaderStream;
-        // read file's buffer contents into streams
-        vShaderStream << vShaderFile.rdbuf();
-        fShaderStream << fShaderFile.rdbuf();
-        // close file handlers
-        vShaderFile.close();
-        fShaderFile.close();
-        // convert stream into string
-        vertexCode = vShaderStream.str();
-        fragmentCode = fShaderStream.str();
-    }
-    catch (std::ifstream::failure& e)
-    {
-        std::cout << "ERROR::SHADER::FILE_NOT_SUCCESSFULLY_READ: " << e.what() << std::endl;
-    }
-    const char* vShaderCode = vertexCode.c_str();
-    const char * fShaderCode = fragmentCode.c_str();
-    // 2. compile shaders
-    unsigned int vertex, fragment;
-    // vertex shader
-    vertex = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertex, 1, &vShaderCode, NULL);
-    glCompileShader(vertex);
-    checkCompileErrors(vertex, "VERTEX");
+    // Enable back face culling
+    glEnable(GL_CULL_FACE);
+//! [2]
 
-    // fragment Shader
-    fragment = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragment, 1, &fShaderCode, NULL);
-    glCompileShader(fragment);
-    checkCompileErrors(fragment, "FRAGMENT");
-
-    // shader Program
-    ID = glCreateProgram();
-    glAttachShader(ID, vertex);
-    glAttachShader(ID, fragment);
-    glLinkProgram(ID);
-    checkCompileErrors(ID, "PROGRAM");
-
-    // delete the shaders as they're linked into our program now and no longer necessary
-    glDeleteShader(vertex);
-    glDeleteShader(fragment);
-    glUseProgram(ID);
+    geometries = new GeometryEngine;
 }
 
 void RtsMap::resizeGL(int w, int h) {
@@ -133,6 +151,8 @@ void RtsMap::resizeGL(int w, int h) {
     if (w != h) {
         resize(std::min(w,h),std::min(w, h));
     }
+    projection.setToIdentity();
+    projection.perspective(45.0, 1.0, 3.0, 7.0);
 }
 
 void RtsMap::paintGL() {
@@ -167,57 +187,85 @@ void RtsMap::paintGL() {
 //
 //
 //    glFlush();
-    glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
-    glLoadIdentity();
+
+////////////////////////////////////////////
+
+//    glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+//    glLoadIdentity();
 //    glTranslatef(  0.0,  0.0, 0.0 );
 
-    glRotatef( 30,  1.0,  0.0,  0.0 );
-    glRotatef( 30,  0.0,  1.0,  0.0 );
-    glUniformMatrix4fv(glGetUniformLocation(ID, "projection"), 1, GL_FALSE, &projection[0][0]);
+//    glRotatef( 30,  1.0,  0.0,  0.0 );
+//    glRotatef( 30,  0.0,  1.0,  0.0 );
+//    model = glm::rotate(model, glm::radians(30.0f), glm::vec3(1.0f, 0.3f, 0.5f));
+//    glUniformMatrix4fv(glGetUniformLocation(ID, "projection"), 1, GL_FALSE, &projection[0][0]);
+//    glUniformMatrix4fv(glGetUniformLocation(ID, "view"), 1, GL_FALSE, &view[0][0]);
+//    glUniformMatrix4fv(glGetUniformLocation(ID, "model"), 1, GL_FALSE, &model[0][0]);
+//
+//
+//    glBindTexture( GL_TEXTURE_2D, texture[filter] );
+//    glBegin( GL_QUADS );
+//    glNormal3f( 0.0, 0.0, 0.5 );
+//    glTexCoord2f( 0.0, 0.0 ); glVertex3f( -0.5, -0.5,  0.5 );
+//    glTexCoord2f( 1.0, 0.0 ); glVertex3f(  0.5, -0.5,  0.5 );
+//    glTexCoord2f( 1.0, 1.0 ); glVertex3f(  0.5,  0.5,  0.5 );
+//    glTexCoord2f( 0.0, 1.0 ); glVertex3f( -0.5,  0.5,  0.5 );
+//
+//    glNormal3f( 0.0, 0.0, -0.5 );
+//    glTexCoord2f( 1.0, 0.0 ); glVertex3f( -0.5, -0.5, -0.5 );
+//    glTexCoord2f( 1.0, 1.0 ); glVertex3f( -0.5,  0.5, -0.5 );
+//    glTexCoord2f( 0.0, 1.0 ); glVertex3f(  0.5,  0.5, -0.5 );
+//    glTexCoord2f( 0.0, 0.0 ); glVertex3f(  0.5, -0.5, -0.5 );
+//
+//    glNormal3f( 0.0, 0.5, 0.0 );
+//    glTexCoord2f( 0.0, 1.0 ); glVertex3f( -0.5,  0.5, -0.5 );
+//    glTexCoord2f( 0.0, 0.0 ); glVertex3f( -0.5,  0.5,  0.5 );
+//    glTexCoord2f( 1.0, 0.0 ); glVertex3f(  0.5,  0.5,  0.5 );
+//    glTexCoord2f( 1.0, 1.0 ); glVertex3f(  0.5,  0.5, -0.5 );
+//
+//    glNormal3f( 0.0, -0.5, 0.0 );
+//    glTexCoord2f( 1.0, 1.0 ); glVertex3f( -0.5, -0.5, -0.5 );
+//    glTexCoord2f( 0.0, 1.0 ); glVertex3f(  0.5, -0.5, -0.5 );
+//    glTexCoord2f( 0.0, 0.0 ); glVertex3f(  0.5, -0.5,  0.5 );
+//    glTexCoord2f( 1.0, 0.0 ); glVertex3f( -0.5, -0.5,  0.5 );
+//
+//    glNormal3f( 0.5, 0.0, 0.0 );
+//    glTexCoord2f( 1.0, 0.0 ); glVertex3f(  0.5, -0.5, -0.5 );
+//    glTexCoord2f( 1.0, 1.0 ); glVertex3f(  0.5,  0.5, -0.5 );
+//    glTexCoord2f( 0.0, 1.0 ); glVertex3f(  0.5,  0.5,  0.5 );
+//    glTexCoord2f( 0.0, 0.0 ); glVertex3f(  0.5, -0.5,  0.5 );
+//
+//    glNormal3f( -0.5, 0.0, 0.0 );
+//    glTexCoord2f( 0.0, 0.0 ); glVertex3f( -0.5, -0.5, -0.5 );
+//    glTexCoord2f( 1.0, 0.0 ); glVertex3f( -0.5, -0.5,  0.5 );
+//    glTexCoord2f( 1.0, 1.0 ); glVertex3f( -0.5,  0.5,  0.5 );
+//    glTexCoord2f( 0.0, 1.0 ); glVertex3f( -0.5,  0.5, -0.5 );
+//    glEnd();
+//    glFlush();
+//    glEnable(GL_LIGHTING);
+//    glDisable(GL_LIGHTING);
+//    updateGeometry();
 
+    // Clear color and depth buffer
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    glBindTexture( GL_TEXTURE_2D, texture[filter] );
-    glBegin( GL_QUADS );
-    glNormal3f( 0.0, 0.0, 0.5 );
-    glTexCoord2f( 0.0, 0.0 ); glVertex3f( -0.5, -0.5,  0.5 );
-    glTexCoord2f( 1.0, 0.0 ); glVertex3f(  0.5, -0.5,  0.5 );
-    glTexCoord2f( 1.0, 1.0 ); glVertex3f(  0.5,  0.5,  0.5 );
-    glTexCoord2f( 0.0, 1.0 ); glVertex3f( -0.5,  0.5,  0.5 );
+    texture->bind();
 
-    glNormal3f( 0.0, 0.0, -0.5 );
-    glTexCoord2f( 1.0, 0.0 ); glVertex3f( -0.5, -0.5, -0.5 );
-    glTexCoord2f( 1.0, 1.0 ); glVertex3f( -0.5,  0.5, -0.5 );
-    glTexCoord2f( 0.0, 1.0 ); glVertex3f(  0.5,  0.5, -0.5 );
-    glTexCoord2f( 0.0, 0.0 ); glVertex3f(  0.5, -0.5, -0.5 );
+//! [6]
+    // Calculate model view transformation
+    QMatrix4x4 matrix;
+    matrix.translate(0.0, 0.0, -5.0);
+    matrix.rotate(rotation);
 
-    glNormal3f( 0.0, 0.5, 0.0 );
-    glTexCoord2f( 0.0, 1.0 ); glVertex3f( -0.5,  0.5, -0.5 );
-    glTexCoord2f( 0.0, 0.0 ); glVertex3f( -0.5,  0.5,  0.5 );
-    glTexCoord2f( 1.0, 0.0 ); glVertex3f(  0.5,  0.5,  0.5 );
-    glTexCoord2f( 1.0, 1.0 ); glVertex3f(  0.5,  0.5, -0.5 );
+    // Set modelview-projection matrix
+    program.setUniformValue("mvp_matrix", projection * matrix);
+//! [6]
 
-    glNormal3f( 0.0, -0.5, 0.0 );
-    glTexCoord2f( 1.0, 1.0 ); glVertex3f( -0.5, -0.5, -0.5 );
-    glTexCoord2f( 0.0, 1.0 ); glVertex3f(  0.5, -0.5, -0.5 );
-    glTexCoord2f( 0.0, 0.0 ); glVertex3f(  0.5, -0.5,  0.5 );
-    glTexCoord2f( 1.0, 0.0 ); glVertex3f( -0.5, -0.5,  0.5 );
+    // Use texture unit 0 which contains cube.png
+    program.setUniformValue("texture", 0);
 
-    glNormal3f( 0.5, 0.0, 0.0 );
-    glTexCoord2f( 1.0, 0.0 ); glVertex3f(  0.5, -0.5, -0.5 );
-    glTexCoord2f( 1.0, 1.0 ); glVertex3f(  0.5,  0.5, -0.5 );
-    glTexCoord2f( 0.0, 1.0 ); glVertex3f(  0.5,  0.5,  0.5 );
-    glTexCoord2f( 0.0, 0.0 ); glVertex3f(  0.5, -0.5,  0.5 );
+    // Draw cube geometry
+    geometries->drawCubeGeometry(&program);
 
-    glNormal3f( -0.5, 0.0, 0.0 );
-    glTexCoord2f( 0.0, 0.0 ); glVertex3f( -0.5, -0.5, -0.5 );
-    glTexCoord2f( 1.0, 0.0 ); glVertex3f( -0.5, -0.5,  0.5 );
-    glTexCoord2f( 1.0, 1.0 ); glVertex3f( -0.5,  0.5,  0.5 );
-    glTexCoord2f( 0.0, 1.0 ); glVertex3f( -0.5,  0.5, -0.5 );
-    glEnd();
-    glFlush();
-    glEnable(GL_LIGHTING);
-    glDisable(GL_LIGHTING);
-    updateGeometry();
 
     this->makeCurrent();
 }
@@ -225,33 +273,33 @@ void RtsMap::paintGL() {
 
 
 
-void RtsMap::loadGLTextures() {
-    QImage tex, buf;
-    if ( !buf.load( "D:/repo/rts/observer/ui/resource/CG人物/星际2- 诺娃nova/[8%OKO@UE(L_X1O[3HWMNZ9.jpg" ) )
-    {
-        qWarning( "Could not read image file, using single-color instead." );
-        QImage dummy( 128, 128, QImage::Format_RGB32);
-        dummy.fill( Qt::green );
-        buf = dummy;
-    }
-    tex = QGLWidget::convertToGLFormat( buf );
-
-    glGenTextures( 3, &texture[0] );
-    glBindTexture( GL_TEXTURE_2D, texture[0] );
-    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST );
-    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST );
-    glTexImage2D( GL_TEXTURE_2D, 0, 3, tex.width(), tex.height(), 0,
-                  GL_RGBA, GL_UNSIGNED_BYTE, tex.bits() );
-    glBindTexture( GL_TEXTURE_2D, texture[1] );
-    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR );
-    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR );
-    glTexImage2D( GL_TEXTURE_2D, 0, 3, tex.width(), tex.height(), 0,
-                  GL_RGBA, GL_UNSIGNED_BYTE, tex.bits() );
-
-    glBindTexture( GL_TEXTURE_2D, texture[2] );
-    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR );
-    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_NEAREST );
-}
+//void RtsMap::loadGLTextures() {
+//    QImage tex, buf;
+//    if ( !buf.load( "D:/repo/rts/observer/ui/resource/CG人物/星际2- 诺娃nova/[8%OKO@UE(L_X1O[3HWMNZ9.jpg" ) )
+//    {
+//        qWarning( "Could not read image file, using single-color instead." );
+//        QImage dummy( 128, 128, QImage::Format_RGB32);
+//        dummy.fill( Qt::green );
+//        buf = dummy;
+//    }
+//    tex = QGLWidget::convertToGLFormat( buf );
+//
+//    glGenTextures( 3, &texture[0] );
+//    glBindTexture( GL_TEXTURE_2D, texture[0] );
+//    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST );
+//    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST );
+//    glTexImage2D( GL_TEXTURE_2D, 0, 3, tex.width(), tex.height(), 0,
+//                  GL_RGBA, GL_UNSIGNED_BYTE, tex.bits() );
+//    glBindTexture( GL_TEXTURE_2D, texture[1] );
+//    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR );
+//    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR );
+//    glTexImage2D( GL_TEXTURE_2D, 0, 3, tex.width(), tex.height(), 0,
+//                  GL_RGBA, GL_UNSIGNED_BYTE, tex.bits() );
+//
+//    glBindTexture( GL_TEXTURE_2D, texture[2] );
+//    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR );
+//    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_NEAREST );
+//}
 
 RtsMap::RtsMap(QWidget *parent): QOpenGLWidget(parent) {
     xRot = yRot = zRot = 0.0;
@@ -263,4 +311,43 @@ RtsMap::RtsMap(QWidget *parent): QOpenGLWidget(parent) {
     light = true;
 }
 
+void RtsMap::initShaders() {
+    // Compile vertex shader
+    if (!program.addShaderFromSourceFile(QOpenGLShader::Vertex, R"(D:\repo\rts\observer\ui\coord_sys.vs)"))
+        close();
 
+    // Compile fragment shader
+    if (!program.addShaderFromSourceFile(QOpenGLShader::Fragment, R"(D:\repo\rts\observer\ui\coord_sys.fs)"))
+        close();
+
+    // Link shader pipeline
+    if (!program.link())
+        close();
+
+    // Bind shader pipeline for use
+    if (!program.bind())
+        close();
+}
+
+void RtsMap::initTextures() {
+    // Load cube.png image
+    texture = new QOpenGLTexture(QImage("D:/repo/rts/observer/ui/resource/CG人物/星际2- 诺娃nova/[8%OKO@UE(L_X1O[3HWMNZ9.jpg"));
+
+    // Set nearest filtering mode for texture minification
+    texture->setMinificationFilter(QOpenGLTexture::Linear);
+
+    // Set bilinear filtering mode for texture magnification
+    texture->setMagnificationFilter(QOpenGLTexture::Linear);
+
+    // Wrap texture coordinates by repeating
+    // f.ex. texture coordinate (1.1, 1.2) is same as (0.1, 0.2)
+    texture->setWrapMode(QOpenGLTexture::Repeat);
+}
+
+
+RtsMap::~RtsMap() {
+    makeCurrent();
+    delete texture;
+    delete geometries;
+    doneCurrent();
+}
--- observer/ui/rts_map.h
+++ observer/ui/rts_map.h
@@ -7,15 +7,23 @@
 #include <QOpenGLWidget>
 #include <QOpenGLFunctions>
 #include <QOpenGLFramebufferObjectFormat>
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
+#include <QMatrix4x4>
+#include <QQuaternion>
+#include <QVector2D>
+#include <QBasicTimer>
+#include <QOpenGLShaderProgram>
+#include <QOpenGLTexture>
+#include "geometry_engine.h"
 class RtsMap : public QOpenGLWidget, protected QOpenGLFunctions {
 public:
     explicit RtsMap(QWidget* parent);
+    ~RtsMap();
 protected:
     void initializeGL() Q_DECL_OVERRIDE;
     void resizeGL(int w, int h) Q_DECL_OVERRIDE;
     void paintGL() Q_DECL_OVERRIDE;
+    void initShaders();
+    void initTextures();
     void graficarLines();
     void loadGLTextures();
     void checkCompileErrors(GLuint shader, std::string type);
@@ -24,7 +32,7 @@ protected:
     GLfloat xRot, yRot, zRot;
     GLfloat zoom;
     GLfloat xSpeed, ySpeed;
-    GLuint texture[3];
+//    GLuint texture[3];
     GLuint filter;
 
     bool light;
@@ -35,6 +43,18 @@ public:
     double z=0;
 
     int w = 0, h = 0;
+
+    QOpenGLShaderProgram program;
+    GeometryEngine *geometries = nullptr;
+
+    QOpenGLTexture *texture = nullptr;
+
+    QMatrix4x4 projection;
+
+    QVector2D mousePressPosition;
+    QVector3D rotationAxis;
+    qreal angularSpeed = 0;
+    QQuaternion rotation;
 };
 
 

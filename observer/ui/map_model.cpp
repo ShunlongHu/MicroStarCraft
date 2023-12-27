b1da8b607e9f35bac298106375d2ce47fd9049a4
zoe50 <zoe50@QIAQIA>
2023-12-27 08:53:21 +0800

    add edge

--- observer/ui/map_model.cpp
+++ observer/ui/map_model.cpp
@@ -23,22 +23,19 @@ bool MapModel::loadModel(const GameState& state)
             color[loc.x + loc.y * state.w] = {0.0,0.0,0.0,0.0};
         }
     }
-    static QMessageBox messageBox;
+    float vEdge = 1.0f / state.h/10;
+    float hEdge = 1.0f / state.w/10;
     for (auto i = 0; i < state.h; ++i) {
         for (auto j = 0; j < state.w; ++j) {
-            float yMin = (float)i * 2.0f / state.h - 1.0f;
-            float yMax = ((float)i * 2.0f + 2.0f) / state.h - 1.0f;
-            float xMin = (float)j * 2.0f / state.h - 1.0f;
-            float xMax = ((float)j * 2.0f + 2.0f) / state.w - 1.0f;
-
-            messageBox.setText(messageBox.text() + QString::number(yMin) + " " +QString::number(yMax) + " " +QString::number(xMin) + " " +QString::number(xMax) + "\n" );
-
+            float yMin = (float)i * 2.0f / state.h - 1.0f + vEdge;
+            float yMax = ((float)i * 2.0f + 2.0f) / state.h - 1.0f - vEdge;
+            float xMin = (float)j * 2.0f / state.h - 1.0f + hEdge;
+            float xMax = ((float)j * 2.0f + 2.0f) / state.w - 1.0f - hEdge;
             vector<QVector3D> coord {{yMin, xMin, 0}, {yMin, xMax, 0}, {yMax, xMax, 0}, {yMax, xMin, 0}};
             vector<GLuint> idx {0,1,2,0,2,3};
             meshes.emplace_back(make_shared<MapMesh>(coord, idx, color[i * state.w + j]));
         }
     }
-    messageBox.show();
     return true;
 }
 void MapModel::draw(QOpenGLShaderProgram *program) {

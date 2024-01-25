c3a6dbd3f4cff1642443511f1c54a99c462dbc0d
zoe50 <zoe50@QIAQIA>
2024-01-25 22:49:45 +0800

    train prints trace

--- client/python_training/train.py
+++ client/python_training/train.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import torch
 from vec_env import *
 from torch.distributions.categorical import Categorical
@@ -19,7 +20,14 @@ def sample(action: torch.Tensor, mask: torch.Tensor) -> torch.Tensor:
 if __name__ == "__main__":
     env = VecEnv(1, torch.device("cpu"), 0, False, True, 0, 5, 5, 100)
     ob, mask = env.reset()
+    torch.manual_seed(0)
     for i in range(1000):
         action1 = torch.rand((env.num_workers, sum(ACTION_SIZE), GAME_H, GAME_W))
         action2 = torch.rand((env.num_workers, sum(ACTION_SIZE), GAME_H, GAME_W))
         o, mask, r, isEnd, t = env.step(sample(action1, mask[0]), sample(action2, mask[1]))
+        plt.imshow(o[0][0, ObPlane.OBSTACLE])
+        ax = plt.gca()
+        ax.set_xticks(np.arange(0, 32, 1))
+        ax.set_yticks(np.arange(0, 32, 1))
+        ax.grid(color='w', linestyle='-', linewidth=1)
+        plt.savefig(f"ob/{i}.png")
